ce62e92ef0f72eba75e923a44fd070473a24801a
xinzhou0714 <zhouxin0714@gmail.com>
2021-12-04 00:22:45 +0100

    2-5 体验Logback的配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        logger.info("Hi you see this info");
+        logger.debug("Hi you cannot see this debug, because debug<info");
     }
 
 }
