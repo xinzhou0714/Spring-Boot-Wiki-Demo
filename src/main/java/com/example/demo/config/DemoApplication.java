e24bb791a860da0e280d9865010b08ea3606849e
xinzhou0714 <zhouxin0714@gmail.com>
2021-12-04 14:23:13 +0100

    2-6 移动主类就找不到之前的controller了

--- src/main/java/com/example/demo/config/DemoApplication.java
+++ src/main/java/com/example/demo/config/DemoApplication.java
@@ -1,10 +1,12 @@
-package com.example.demo;
+package com.example.demo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class DemoApplication {
