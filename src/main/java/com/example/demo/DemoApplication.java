49818d81d330458f24c93b01c08ec5861dd97111
xinzhou0714 <zhouxin0714@gmail.com>
2021-12-04 01:08:23 +0100

    2-5 修改启动文案， env只是通过它能拿到环境变量罢了

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,15 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-        logger.info("Hi you see this info");
-        logger.debug("Hi you cannot see this debug, because debug<info");
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        logger.info("app started successfully ! ! !");
+        logger.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+        //logger.info("Hi you see this info");
+        //logger.debug("Hi you cannot see this debug, because debug<info");
     }
 
 }
