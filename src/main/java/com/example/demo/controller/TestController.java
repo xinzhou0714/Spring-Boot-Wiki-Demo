c877c10ed299def890e7ad10c08e60cb131261dd
xinzhou0714 <zhouxin0714@gmail.com>
2021-12-04 16:24:19 +0100

    2-7 增加hallo 接口测试test.http

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,17 +1,29 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     /**
      * GET, POST,PUT, DELETE
      */
+    /*
+    *可供选择的注解
+    *@PostMapping
+    *@PutMapping
+    *@DeleteMapping
+    *@RequestMapping(value = "/user/1",method = RequestMethod.GET)
+    *@RequestMapping(value = "/user/1",method = RequestMethod.POST)
+     */
     @RequestMapping("/hallo")
     public String hallo(@RequestParam String id){
         return "Hallo Welt:"+ id;
     }
+
+    @PostMapping("/hallo/post")
+    public String halloPost(String id,String name){
+        return "Hallo Welt! Post \nId: "+ id+ "\nName: "+name;
+    }
+
 
 }
